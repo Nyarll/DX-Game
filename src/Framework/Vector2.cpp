566f9ae21b8078116a4cfbb6944a2c496e82f404
UNLUCKY <oss314y08@i.softbank.jp>
2023-10-13 17:22:06 +0900

    Vector2

---
+++ src/Framework/Vector2.cpp
@@ -0,0 +1,110 @@
+#include "Vector2.h"
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+Vector2 Vector2::Normalized()
+{
+	float len = this->Length();
+	return{ this->x / len,this->y / len };
+}
+
+float Vector2::Dot(Vector2* vec2)
+{
+	return ((this->x * vec2->x) + (this->y * vec2->y));
+}
+
+float Vector2::Cross(Vector2* vec2)
+{
+	return (this->x * vec2->y - vec2->x * this->y);
+}
+
+Vector2 Vector2::Rotate(float rad)
+{
+	return{ ((this->x * cosf(rad)) - (this->y * sinf(rad))),((this->x * sinf(rad)) + (this->y * cosf(rad))) };
+}
+
+float Vector2::Angle()
+{
+	return atan2f(this->y, this->x);
+}
+
+float Vector2::Angle2(Vector2* vec2)
+{
+	return atan2(vec2->y - this->y, vec2->x - this->x);
+}
+
+float Vector2::Length()
+{
+	return sqrtf(this->x * this->x + this->y * this->y);
+}
+
+float Vector2::LengthSquared()
+{
+	return (this->x * this->x) + (this->y * this->y);
+}
+
+float Vector2::LengthTo(Vector2* vec2)
+{
+	return sqrtf(LengthSquaredTo(vec2));
+}
+
+float Vector2::LengthSquaredTo(Vector2* vec2)
+{
+	return (vec2->x - this->x) * (vec2->x - this->x) + (vec2->y - this->y) * (vec2->y - this->y);
+}
+
+void Vector2::Decompose(Vector2* vec2, Vector2* outvec1, Vector2* outvec2)
+{
+	float angle_rota = vec2->Angle();
+	float vec_rota = Angle();
+	float diff_rota = vec_rota - angle_rota;
+	float vec_length = Length();
+
+	float vec_a_length = vec_length * cosf(diff_rota);
+	float vec_b_length = vec_length * sinf(diff_rota);
+	float vec_a_rota = angle_rota;
+	float vec_b_rota = angle_rota + (float)M_PI_2;
+
+	*outvec1 = { vec_a_length * cosf(vec_a_rota), vec_a_length * sinf(vec_a_rota) };
+	*outvec2 = { vec_b_length * cosf(vec_b_rota), vec_b_length * sinf(vec_b_rota) };
+}
+
+Vector2 Vector2::TranslateTowards(const Vector2& from, const Vector2& to, float maxDelta)
+{
+	Vector2 sub = to - from;
+	Vector2 vel = sub.Normalized() * maxDelta;
+	Vector2 towards = (vel.LengthSquared() < sub.LengthSquared()) ? vel : sub;
+	return from + towards;
+}
+
+bool Vector2::CrossPoint(Vector2* vec1_gool, Vector2* vec2_start, Vector2* vec2_gool, Vector2* output)
+{
+	Vector2 p1 = { this->x,this->y };
+	Vector2 p2 = *vec1_gool;
+	Vector2 p3 = *vec2_start;
+	Vector2 p4 = *vec2_gool;
+
+	float d = (p2.x - p1.x) * (p4.y - p3.y) - (p2.y - p1.y) * (p4.x - p3.x);
+	if (d == 0)
+	{
+		return false;
+	}
+	float u = ((p3.x - p1.x) * (p4.y - p3.y) - (p3.y - p1.y) * (p4.x - p3.x)) / d;
+	float v = ((p3.x - p1.x) * (p2.y - p1.y) - (p3.y - p1.y) * (p2.x - p1.x)) / d;
+
+	// このブロックを消すと、直線同士の交点になります
+	{
+		if (u < 0.0 || u > 1.0)
+		{
+			return false;
+		}
+		if (v < 0.0 || v > 1.0)
+		{
+			return false;
+		}
+	}
+	*output = { p1.x + u * (p2.x - p1.x) ,p1.y + u * (p2.y - p1.y) };
+
+	return true;
+}
---
+++ src/Framework/Vector2.h
@@ -0,0 +1,97 @@
+// ===== ===== ===== ===== ===== ===== ===== =====
+// @file	Vector2.h
+// @brief	2D ベクトル
+// 
+// @date	2023-10-13
+// @update	2023-10-13
+// 
+// @author	UNLUCKY
+// ===== ===== ===== ===== ===== ===== ===== =====
+
+#pragma once
+
+class Vector2
+{
+private:
+	float x, y;
+
+public:
+	Vector2();
+	Vector2(const Vector2& vec);
+	Vector2(float x, float y);
+	Vector2(int x, int y);
+
+	inline Vector2 operator +() const { return *this; }
+	inline Vector2 operator -() const { return (*this) * (-1); }
+
+	inline Vector2 operator +(const Vector2& other) const { return { x + other.x, y + other.y }; }
+	inline Vector2 operator -(const Vector2& other) const { return { x - other.x, y - other.y }; }
+	inline Vector2 operator *(const Vector2& scale) const { return { x * scale.x, y * scale.y }; }
+	inline Vector2 operator *(const float scale) const { return { x * scale, y * scale }; }
+	inline Vector2 operator /(const Vector2& scale) const { return { x / scale.x, y / scale.y }; }
+	inline Vector2 operator /(const float scale) const { return { x / scale, y / scale }; }
+
+	inline Vector2& operator +=(const Vector2& other)
+	{
+		x += other.x;
+		y += other.y;
+		return *this;
+	}
+
+	inline Vector2& operator -=(const Vector2& other)
+	{
+		x -= other.x;
+		y -= other.y;
+		return *this;
+	}
+
+	inline Vector2& operator *=(const Vector2& scale)
+	{
+		x *= scale.x;
+		y *= scale.y;
+		return *this;
+	}
+
+	inline Vector2& operator *=(const float scale)
+	{
+		x *= scale;
+		y *= scale;
+		return *this;
+	}
+
+	inline Vector2& operator /=(const Vector2& scale)
+	{
+		x /= scale.x;
+		y /= scale.y;
+		return *this;
+	}
+
+	inline Vector2& operator /=(const float scale)
+	{
+		x /= scale;
+		y /= scale;
+		return *this;
+	}
+
+	bool operator==(const Vector2& p) { return ((static_cast<int>(this->x) == static_cast<int>(p.x)) && (static_cast<int>(this->y) == static_cast<int>(p.y))); };
+	bool operator!=(const Vector2& p) { return ((static_cast<int>(this->x) != static_cast<int>(p.x)) && (static_cast<int>(this->y) != static_cast<int>(p.y))); };
+
+	Vector2 Normalized();				// 2Dベクトルの正規化
+	float Dot(Vector2* vec);	// 2Dベクトル同士の内積
+	float Cross(Vector2* vec);	// 2Dベクトル同士の外積
+
+	Vector2 Rotate(float rad);	// 2Dベクトルをrad分回転させる
+
+	float Angle();					// 2Dベクトルの角度
+	float Angle2(Vector2* vec);	// 2Dベクトル間の角度
+	float Length();					// 2Dベクトルの長さ
+	float LengthSquared();			// 2Dベクトルの長さの二乗
+	float LengthTo(Vector2* vec);// 2Dベクトル間の距離
+	float LengthSquaredTo(Vector2* vec);	// 2Dベクトル間の距離の二乗
+
+	void Decompose(Vector2* vec, Vector2* outvec1, Vector2* outvec2);	// 2Dベクトルを分解
+
+	Vector2 TranslateTowards(const Vector2& from, const Vector2& to, float maxDelta);
+
+	bool CrossPoint(Vector2* vec1_gool, Vector2* vec2_start, Vector2* vec2_gool, Vector2* output);// 線分と線分の交点を求める
+};
